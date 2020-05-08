3008306cd6e66c68606e6645866ac2fa59c947b2
Emily <emilymnicholas@gmail.com>
2020-05-08 05:37:01 +0000

    promises

---
+++ index2.js
@@ -0,0 +1,29 @@
+const request = require('request-promise-native');
+
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const nextTime = new Date();
+    nextTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${nextTime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })   
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
